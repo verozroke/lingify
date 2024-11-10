fe1b6b32cb188b17cf64f323ee6ce49e5d2ce665
verozroke <73778760+verozroke@users.noreply.github.com>
2024-11-10 18:50:02 +0500

    createCouseAsTeacher method added

--- components/course/CreateDialog.vue
+++ components/course/CreateDialog.vue
@@ -1,8 +1,137 @@
 <template>
-  <v-dialog v-model="dialog" width="500px">
-    <v-card :loading="isLoading" style="padding: 16px">
-      <v-card-title> Create Course </v-card-title>
-      <v-form @submit="createCourse" class="p-4 flex flex-col gap-2">
+  <v-dialog
+    v-model="dialog"
+    width="500px"
+  >
+    <v-card
+      :loading="isLoading"
+      style="padding: 16px"
+    >
+      <v-card-title> Create Course for {{ isTeacher ? 'Teacher' : 'Students' }} </v-card-title>
+      <v-switch
+        v-model="isTeacher"
+        class="px-4"
+        :color="colors.SEAGREEN"
+        :label="isTeacher ? 'I\'m teacher' : 'I\'m student'"
+      ></v-switch>
+      <v-form
+        v-if="isTeacher"
+        @submit="createCourseAsTeacher"
+        class="p-4 flex flex-col gap-2"
+      >
+        <UiInput
+          :loading="isLoading"
+          :disabled="isLoading"
+          :color="colors.EMERALD"
+          label="Course Language"
+          v-model="teacherInputs.courseLanguage"
+          :rules="teacherRules.courseLanguage"
+          placeholder="Type course lanuage."
+          type="text"
+        />
+        <UiInput
+          :loading="isLoading"
+          :disabled="isLoading"
+          :color="colors.EMERALD"
+          label="Native Language"
+          v-model="teacherInputs.nativeLanguage"
+          :rules="teacherRules.nativeLanguage"
+          placeholder="Type your native lanuage."
+          type="text"
+        />
+        <v-select
+          label="Language Level"
+          v-model="teacherInputs.languageLevel"
+          :items="levels"
+          variant="outlined"
+        >
+        </v-select>
+        <UiInput
+          :loading="isLoading"
+          :disabled="isLoading"
+          :color="colors.EMERALD"
+          label="Course Description"
+          v-model="teacherInputs.description"
+          :rules="teacherRules.description"
+          placeholder="Type course description."
+          type="text"
+        />
+        <UiInput
+          :loading="isLoading"
+          :disabled="isLoading"
+          :color="colors.EMERALD"
+          label="URL Аватарки"
+          v-model="teacherInputs.avatarUrl"
+          :rules="teacherRules.avatarUrl"
+          placeholder="Введите URL аватарки."
+          type="text"
+        />
+        <v-btn
+          :disable="isLoading"
+          :loading="isLoading"
+          @click="addLesson"
+          mode="elevated"
+          :color="colors.EMERALD"
+          text-color="white"
+          style="text-transform: none;"
+          prepend-icon="mdi-plus"
+        >Add Lesson</v-btn>
+        <template v-for="(lesson, i) in teacherInputs.lessons">
+          <h1 class="py-4 font-medium text-xl"> Lesson {{ i + 1 }} {{ lesson.name ? `(${lesson.name})` : '' }} </h1>
+          <UiInput
+            :loading="isLoading"
+            :disabled="isLoading"
+            :color="colors.EMERALD"
+            label="Lesson Name"
+            v-model="lesson.name"
+            placeholder="Type lesson name."
+            type="text"
+          />
+
+          <UiInput
+            :loading="isLoading"
+            :disabled="isLoading"
+            :color="colors.EMERALD"
+            label="Lesson Description"
+            v-model="lesson.description"
+            placeholder="Type lesson description."
+            type="text"
+          />
+          <UiInput
+            :loading="isLoading"
+            :disabled="isLoading"
+            :color="colors.EMERALD"
+            label="Lesson Key Words"
+            v-model="lesson.keyWords"
+            placeholder="Type lesson key words."
+            type="text"
+          />
+          <UiInput
+            :loading="isLoading"
+            :disabled="isLoading"
+            :color="colors.EMERALD"
+            label="Lesson Material Name"
+            v-model="lesson.materialName"
+            placeholder="Type material name."
+            type="text"
+          />
+          <v-textarea
+            :loading="isLoading"
+            :disabled="isLoading"
+            :color="colors.EMERALD"
+            variant="outlined"
+            label="Lesson Material Text"
+            v-model="lesson.materialText"
+            placeholder="Type material text."
+            type="text"
+          />
+        </template>
+      </v-form>
+      <v-form
+        v-if="!isTeacher"
+        @submit="createCourse"
+        class="p-4 flex flex-col gap-2"
+      >
         <UiInput
           :loading="isLoading"
           :disabled="isLoading"
@@ -46,13 +175,12 @@
           :disable="isLoading"
           :loading="isLoading"
           type="submit"
-          @click="createCourse"
+          @click="isTeacher ? createCourseAsTeacher() : createCourse()"
           mode="elevated"
           :color="colors.EMERALD"
           text-color="white"
           prepend-icon="mdi-account-multiple-plus"
-          >Создать</UiButton
-        >
+        >Создать</UiButton>
         <UiButton
           :disable="isLoading"
           :loading="isLoading"
@@ -61,8 +189,7 @@
           mode="tonal"
           color="red"
           prepend-icon="mdi-close"
-          >Закрыть</UiButton
-        >
+        >Закрыть</UiButton>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -71,15 +198,33 @@
 <script setup lang="ts">
 import { colors } from "~/core/colors";
 import { useToast } from "~/hooks/use-toast";
-import type { CreateCoursePayload } from "~/services/course.service";
+import type { CreateCourseAsTeacherPayload, CreateCoursePayload } from "~/services/course.service";
 import courseService from "~/services/course.service";
+
+
+export type LessonInput = {
+  name: string
+  description: string
+  keyWords: string
+  materialName: string
+  materialText: string
+}
+
+export type TeacherInputsType = {
+  courseLanguage: string,
+  nativeLanguage: string,
+  languageLevel: string,
+  avatarUrl: string,
+  description: string,
+  lessons: LessonInput[],
+}
 
 const router = useRouter();
 const { toast } = useToast();
 const userStore = useUserStore();
 const dialog = defineModel<boolean>();
 const isLoading = ref(false);
-
+const isTeacher = ref(false)
 const levels = ref<string[]>(["A1", "A2", "B1", "B2", "C1", "C2"]);
 
 const inputs = ref({
@@ -88,6 +233,54 @@ const inputs = ref({
   languageLevel: "",
   avatarUrl: "",
 });
+
+const teacherInputs = ref<TeacherInputsType>({
+  courseLanguage: "",
+  nativeLanguage: "",
+  languageLevel: "",
+  avatarUrl: "",
+  description: "",
+  lessons: [],
+});
+
+
+const addLesson = () => {
+  teacherInputs.value.lessons.push({
+    name: '',
+    description: '',
+    keyWords: '',
+    materialName: '',
+    materialText: '',
+  })
+}
+const createCourseAsTeacher = async () => {
+  if (!userStore.user) {
+    return;
+  }
+  const { courseLanguage, nativeLanguage, languageLevel, avatarUrl, description, lessons } =
+    teacherInputs.value;
+
+  const payload: CreateCourseAsTeacherPayload = {
+    courseLanguage,
+    nativeLanguage,
+    languageLevel,
+    userId: userStore.user.id,
+    avatarUrl,
+    description,
+    lessons,
+  };
+
+  try {
+    isLoading.value = true;
+    const course = await courseService.createCourseAsTeacher(payload);
+    toast.success({ message: "Course created as teacher successfully." });
+    router.push(`/courses/${course.id}`);
+    isLoading.value = false;
+  } catch (error) {
+    toast.error({ message: "Could not create course as teacher." });
+    isLoading.value = false;
+  }
+}
 
 const createCourse = async () => {
   if (!userStore.user) {
@@ -124,6 +317,18 @@ const rules = ref({
     (v: string) => !!v || "avatarUrl is required.",
     (v: any) => isValidUrl(v) || "Must be URL",
   ],
+});
+
+
+const teacherRules = ref({
+  courseLanguage: [(v: string) => !!v || "Course Language is required."],
+  nativeLanguage: [(v: string) => !!v || "Native language is required."],
+  languageLevel: [(v: string) => !!v || "Language level is required."],
+  avatarUrl: [
+    (v: string) => !!v || "avatarUrl is required.",
+    (v: any) => isValidUrl(v) || "Must be URL",
+  ],
+  description: [(v: string) => !!v || "Description is required."],
 });
 </script>
 
--- services/course.service.ts
+++ services/course.service.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import type { LessonInput } from "~/components/course/CreateDialog.vue";
 import type { Course } from "~/core/types/course";
 
 export type CreateCoursePayload = {
@@ -8,6 +9,16 @@ export type CreateCoursePayload = {
   userId: string;
   avatarUrl: string;
 };
+
+export type CreateCourseAsTeacherPayload = {
+  courseLanguage: string,
+  nativeLanguage: string,
+  languageLevel: string,
+  avatarUrl: string,
+  description: string,
+  userId: string;
+  lessons: LessonInput[],
+}
 
 class CourseService {
   baseUrl: string;
@@ -45,6 +56,17 @@ class CourseService {
 
   async createCourse(body: CreateCoursePayload): Promise<Course> {
     const { data } = await axios.post<Course>(`${this.baseUrl}/courses`, body, {
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return data;
+  }
+
+  async createCourseAsTeacher(body: CreateCourseAsTeacherPayload): Promise<Course> {
+    const { data } = await axios.post<Course>(`${this.baseUrl}/courses/as-teacher`, body, {
       withCredentials: true,
       headers: {
         "Content-Type": "application/json",
